8b63c744a3d3a3f3bb103a4c3aa0ecc5b11182a5
Doosik Kim <doosik@etri.re.kr>
2023-11-13 10:35:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,68 @@
-const fs = require('fs');
+// app.js
+const express = require('express');
 const pdf = require('pdf-parse');
-const copyPaste = require('copy-paste');
 
-if (process.argv.length < 3) {
-  console.log("Usage: node pdf2text.js <file_path>");
-  process.exit(1);
-}
+const app = express();
+const port = 3000;
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                margin-top: 2em;
+            }
+            #url {
+              width: 40em;
+            }
+          </style>
+        <title>PDF2Text</title>
+    </head>
+    <body>
+        <form action="/pdf" method="get">
+            <label for="url">PDF URL:</label>
+            <input type="url" id="url" name="url" spellcheck="false"placeholder="Input PDF url here!" required>
+            <button type="submit">Submit</button>
+            <p>PDF example: <a href="pdf?url=https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf">https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf</a></p>
+        </form>
+    </body>
+</html>
+`;
 
-const dataBuffer = fs.readFileSync(process.argv[2]);
+// Define a route
+app.get('/', (req, res) => {
+  res.send(html);
+});
 
-const extractTextFromPDF = async (pdfBuffer) => {
+// Handle the GET request with query parameters
+app.get('/pdf', async (req, res) => {
+  const pdfUrl = req.query.url;
+  const extractedText = await extractTextFromPdf(pdfUrl);
+  res.send(extractedText);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+async function extractTextFromPdf(url) {
   try {
-    const data = await pdf(pdfBuffer);
-    return data.text;
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-extractTextFromPDF(dataBuffer)
-  .then((text) => {
-    if (text) {
-      copyPaste.copy(text, () => {
-        console.log(text);
-      });
-    } else {
-      console.log('Failed to extract text from the PDF.');
-      process.exit(1);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch PDF. Status: ${response.status}`);
     }
-  })
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+
+    const arrayBuffer = await response.arrayBuffer();
+    const extractedText = await pdf(arrayBuffer);
+
+    return extractedText.text;
+  } catch (error) {
+    return `[Error] "${error}"`;
+  }
+}
